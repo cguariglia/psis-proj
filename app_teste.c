53463092c737b9cf72206fe16893c3dbb58080f8
Marcelo Cabral <marcelo.cabral@sapo.pt>
2018-05-02 13:20:06 +0000

    git conneeeeeeeeeect

--- app_teste.c
+++ app_teste.c
@@ -1,20 +1,30 @@
-#include "clipboard.h"
-#include <sys/types.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+#include "clipboard.h"
 int main(){
+    int fd = clipboard_connect(SERVER_ADDRESS);
 
-		int fd = clipboard_connect("./");
+    if(fd == -1){
+        exit(-1);
+    }
 
-		if(fd== -1){
-			exit(-1);
-		}
-		char dados[10];
-		int dados_int;
-		fgets(dados, 10, stdin);
-		/*write(fd, dados, 10);
-		read(fd+1, &dados_int, sizeof(dados_int));*/
-		printf("Received %d\n", dados_int);
+    char *data1 = "asdf";
+    char *data2 = "qwerty";
+    int region1 = 2;
+    int region2 = 5;
+    char str[5];
+    int bytes;
 
-		exit(0);
-	}
+    printf("Copying \"%s\" to clipboard region %d\n", data1, region1);
+    bytes = clipboard_copy(fd, region1, (void *) data1, sizeof(data1));
+    printf("Copied %d bytes to clipboard\n", bytes);
+    printf("Copying \"%s\" to clipboard region %d\n", data2, region2);
+    bytes = clipboard_copy(fd, region2, (void *) data2, sizeof(data2));
+    printf("Copied %d bytes to clipboard\n", bytes);
+    printf("Pasting data from clipboard region %d\n", region1);
+    bytes = clipboard_paste(fd, region2, (void *) str, sizeof(str));
+    printf("Pasted \"%s\", %d bytes\n", str, bytes);
+
+    exit(0);
+}
